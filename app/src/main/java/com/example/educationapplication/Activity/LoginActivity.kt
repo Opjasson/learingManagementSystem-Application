a0272a0d59e40332251c9a4f2877a1300cb516de
Opjasson <azmitheusos333@gmail.com>
2025-12-22 22:31:10 +0700

    done

--- app/src/main/java/com/example/educationapplication/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/educationapplication/Activity/LoginActivity.kt
@@ -1,25 +1,21 @@
 package com.example.educationapplication.Activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.educationapplication.R
-import com.example.educationapplication.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding= ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.splashBtn.setOnClickListener {
-        startActivity(Intent(this, LoginActivity::class.java))
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
