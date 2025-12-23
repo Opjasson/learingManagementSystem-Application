1f245e64c17cc1c0098d4bf44bfdbb51f33520f0
Opjasson <azmitheusos333@gmail.com>
2025-12-23 09:12:18 +0700

    MainActivity : done setting view design search bar

--- app/src/main/java/com/example/educationapplication/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/educationapplication/Activity/LoginActivity.kt
@@ -1,21 +1,33 @@
 package com.example.educationapplication.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.educationapplication.R
+import com.example.educationapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
 }
