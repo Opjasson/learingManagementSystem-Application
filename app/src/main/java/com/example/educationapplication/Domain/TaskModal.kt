9c3756e98b1811455bfa635d156ce54c4484a6c3
Opjasson <azmitheusos333@gmail.com>
2025-12-29 14:38:01 +0700

    TaskModal : create dataClass taskModal

---
+++ app/src/main/java/com/example/educationapplication/Domain/TaskModal.kt
@@ -0,0 +1,13 @@
+package com.example.educationapplication.Domain
+
+import java.io.Serializable
+
+data class TaskModal(
+    var lessonId: Number = 0,
+    var soal: String = "",
+    var pilihan1: String = "",
+    var pilihan2: String = "",
+    var pilihan3: String = "",
+    var pilihan4: String = "",
+    var answer: String = "",
+) : Serializable
