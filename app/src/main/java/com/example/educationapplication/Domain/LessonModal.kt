d8989494e36254ef74b0795b68eae3a147733045
Opjasson <azmitheusos333@gmail.com>
2025-12-29 14:29:20 +0700

    LessonModal : add lessonId

--- app/src/main/java/com/example/educationapplication/Domain/LessonModal.kt
+++ app/src/main/java/com/example/educationapplication/Domain/LessonModal.kt
@@ -3,6 +3,7 @@ package com.example.educationapplication.Domain
 import java.io.Serializable
 
 data class LessonModal(
+    var lessonId: Number=0,
     var name: String="",
     var description: String=""
 ) : Serializable
