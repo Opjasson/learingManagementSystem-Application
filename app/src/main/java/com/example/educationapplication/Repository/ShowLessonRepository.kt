9ec10a98a691825a6bac7cd8d7b86a6393c32e40
Opjasson <azmitheusos333@gmail.com>
2025-12-31 09:34:05 +0700

    Repository : create ShowLessonRepository

---
+++ app/src/main/java/com/example/educationapplication/Repository/ShowLessonRepository.kt
@@ -0,0 +1,13 @@
+package com.example.educationapplication.Repository
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ShowLessonRepository {
+//    database
+    private val database = FirebaseFirestore.getInstance()
+
+//    get all data task
+fun loadTask() {
+
+}
+}
